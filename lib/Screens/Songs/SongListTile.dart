b5725fd98acea264d23df32a9af81fb83baa4aed
Ahmed Afridee <a.afridee47@gmail.com>
2021-02-08 22:39:24 +0600

    second push

--- lib/Screens/Songs/SongListTile.dart
+++ lib/Screens/Songs/SongListTile.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_audio_query/flutter_audio_query.dart';
+import 'package:get/get.dart';
+import 'package:musicplayer/Controllers/AudioQuerying.dart';
 
 class SongListTile extends StatelessWidget {
 
   final SongInfo songInfo;
 
+
   const SongListTile({Key key, this.songInfo}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    final audioQuerying = Get.put(AudioQuerying());
+
     return ListTile(
       leading: Container(
         height: 50,
@@ -22,6 +28,7 @@ class SongListTile extends StatelessWidget {
         ),
       ),
       title: Text(songInfo.title),
+      subtitle: Text(audioQuerying.changeMillisecondsToTime(Duration(milliseconds: int.parse(songInfo.duration)))),
     );
   }
 }
